8ad8693c4f1131bf887d44e79d612b4b8b2bafd8
Cameron Lee <cal26@sfu.ca>
2023-12-11 20:22:30 -0800

    began on flashcard animations

--- app/pages/flashcards-activity/[chapter]/[topic]/page.tsx
+++ app/pages/flashcards-activity/[chapter]/[topic]/page.tsx
@@ -1,6 +1,8 @@
 'use client'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ContentClass } from "@/app/utils/utils";
+import Flashcard from '@/app/components/Flashcard/Flashcard';
+import styles from './FlashcardsActivity.module.css'
 
 function FlashcardsActivity({ 
   params,
@@ -10,18 +12,27 @@ function FlashcardsActivity({
   const selectedChapterStr = params.chapter.replaceAll('%20', ' ')
   const selectedTopicStr = params.topic.replaceAll('%20', ' ')
 
-  let content
+  const content = new ContentClass().get(selectedChapterStr, selectedTopicStr)
+
+  const [animationClass, setAnimationClass] = useState('');
+
   useEffect(() => {
-    content = new ContentClass().get(selectedChapterStr, selectedTopicStr)
-    console.log(content)
+    // Add a class to trigger the animation after a delay
+    const animationTimeout = setTimeout(() => {
+      setAnimationClass(styles.moveAround);
+    }, 0); 
+
+    return () => clearTimeout(animationTimeout);
   }, [])
 
   return (
     <main className='main-center'>
       <h1 className='text-5xl font-semibold'>{selectedChapterStr} {selectedTopicStr} Flashcards</h1>
-      <p></p>
+      <div className={`${animationClass} ${styles.flashcardDiv}`}>
+        <Flashcard frontContent={content[0].japanese} backContent={content[0].english}></Flashcard>
+      </div>
     </main>
-  );
+  )
 }
 
 export default FlashcardsActivity;
