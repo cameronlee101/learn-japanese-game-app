3181ecf535d83c2d56af9db9711ffefce3c80578
Cameron Lee <cal26@sfu.ca>
2024-04-07 21:15:50 -0700

    installed husky with prettier

--- app/pages/contents-of/[chapter]/[topic]/page.tsx
+++ app/pages/contents-of/[chapter]/[topic]/page.tsx
@@ -1,103 +1,103 @@
 "use client";
 import {
-  Content,
-  fetchContent,
-  getFullExampleContentObject,
+	Content,
+	fetchContent,
+	getFullExampleContentObject,
 } from "@/app/utils/content-utils";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 
 function ContentsOf({
-  params,
+	params,
 }: {
-  params: { chapter: string; topic: string };
+	params: { chapter: string; topic: string };
 }) {
-  const router = useRouter();
-  const selectedChapterStr = decodeURI(params.chapter);
-  const selectedTopicStr = decodeURI(params.topic);
+	const router = useRouter();
+	const selectedChapterStr = decodeURI(params.chapter);
+	const selectedTopicStr = decodeURI(params.topic);
 
-  const { status, data, error } = useQuery({
-    queryKey: ["content", selectedChapterStr, selectedTopicStr],
-    queryFn: () => fetchContent(selectedChapterStr, selectedTopicStr),
-  });
+	const { status, data, error } = useQuery({
+		queryKey: ["content", selectedChapterStr, selectedTopicStr],
+		queryFn: () => fetchContent(selectedChapterStr, selectedTopicStr),
+	});
 
-  const [allParameters, setAllParameters] = useState<string[]>([]);
-  const [tableHeaders, setTableHeaders] = useState<string[]>([]);
-  const [contents, setContents] = useState<Content[]>([
-    { japanese: "Loading...", english: "Loading..." },
-  ]);
+	const [allParameters, setAllParameters] = useState<string[]>([]);
+	const [tableHeaders, setTableHeaders] = useState<string[]>([]);
+	const [contents, setContents] = useState<Content[]>([
+		{ japanese: "Loading...", english: "Loading..." },
+	]);
 
-  // Updates the contents based on the result of the react query
-  useEffect(() => {
-    if (error) {
-      console.error(error);
-      alert("Error retrieving contents, returning to home page");
-      router.push("/");
-    }
-    if (data) {
-      setContents(data);
-    }
-  }, [data]);
+	// Updates the contents based on the result of the react query
+	useEffect(() => {
+		if (error) {
+			console.error(error);
+			alert("Error retrieving contents, returning to home page");
+			router.push("/");
+		}
+		if (data) {
+			setContents(data);
+		}
+	}, [data, error]);
 
-  // Extracts parameter names from an example object for the table headers and to align table data
-  useEffect(() => {
-    if (getFullExampleContentObject(contents[0])) {
-      if (getFullExampleContentObject(contents[0])) {
-        setAllParameters(
-          Object.keys(getFullExampleContentObject(contents[0])!),
-        );
+	// Extracts parameter names from an example object for the table headers and to align table data
+	useEffect(() => {
+		if (getFullExampleContentObject(contents[0])) {
+			if (getFullExampleContentObject(contents[0])) {
+				setAllParameters(
+					Object.keys(getFullExampleContentObject(contents[0])!)
+				);
 
-        let objAttributes = Object.keys(
-          getFullExampleContentObject(contents[0])!,
-        );
-        for (let i in objAttributes) {
-          const no_spaces = objAttributes[i].replaceAll("_", " ");
-          objAttributes[i] =
-            no_spaces.charAt(0).toUpperCase() + no_spaces.slice(1);
-        }
-        setTableHeaders(objAttributes);
-      } else {
-        console.error(
-          "Error occurred when retrieving example object containing all attributes",
-        );
-      }
-    }
-  }, [contents]);
+				let objAttributes = Object.keys(
+					getFullExampleContentObject(contents[0])!
+				);
+				for (let i in objAttributes) {
+					const no_spaces = objAttributes[i].replaceAll("_", " ");
+					objAttributes[i] =
+						no_spaces.charAt(0).toUpperCase() + no_spaces.slice(1);
+				}
+				setTableHeaders(objAttributes);
+			} else {
+				console.error(
+					"Error occurred when retrieving example object containing all attributes"
+				);
+			}
+		}
+	}, [contents]);
 
-  return (
-    <main className="main-center">
-      <h1 className="text-5xl font-semibold mb-8">
-        Contents of {selectedChapterStr} {selectedTopicStr}
-      </h1>
-      <div className="overflow-x-auto w-full">
-        <table className="table table-striped-columns table-hover table-bordered table-sm">
-          <thead>
-            <tr>
-              {tableHeaders.map((parameter, index) => (
-                <th key={index} style={{ minWidth: "200px" }}>
-                  {parameter}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody className="table-group-divider">
-            {contents.map((item, rowIndex) => (
-              <tr key={rowIndex}>
-                {allParameters.map((parameter, columnIndex) => (
-                  <td key={columnIndex} className="whitespace-pre">
-                    {Array.isArray(item[parameter])
-                      ? (item[parameter] as string[]).join(",   ")
-                      : item[parameter] || ""}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </main>
-  );
+	return (
+		<main className="main-center">
+			<h1 className="text-5xl font-semibold mb-8">
+				Contents of {selectedChapterStr} {selectedTopicStr}
+			</h1>
+			<div className="overflow-x-auto w-full">
+				<table className="table table-striped-columns table-hover table-bordered table-sm">
+					<thead>
+						<tr>
+							{tableHeaders.map((parameter, index) => (
+								<th key={index} style={{ minWidth: "200px" }}>
+									{parameter}
+								</th>
+							))}
+						</tr>
+					</thead>
+					<tbody className="table-group-divider">
+						{contents.map((item, rowIndex) => (
+							<tr key={rowIndex}>
+								{allParameters.map((parameter, columnIndex) => (
+									<td key={columnIndex} className="whitespace-pre">
+										{Array.isArray(item[parameter])
+											? (item[parameter] as string[]).join(",   ")
+											: item[parameter] || ""}
+									</td>
+								))}
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</main>
+	);
 }
 
 export default ContentsOf;
--- app/pages/selection/[activity]/page.tsx
+++ app/pages/selection/[activity]/page.tsx
@@ -9,187 +9,187 @@ import ErrorBox from "@/app/components/ErrorBox/ErrorBox";
 import { useQuery } from "@tanstack/react-query";
 
 const titleSuffixes = [
-  {
-    activity: "flashcards-activity",
-    suffix: "for Flashcards",
-  },
-  {
-    activity: "mc-quiz",
-    suffix: "for Multiple Choice Quiz",
-  },
-  {
-    activity: "matching-activity",
-    suffix: "for Term Matching",
-  },
-  {
-    activity: "contents-of",
-    suffix: "to See The Contents of",
-  },
+	{
+		activity: "flashcards-activity",
+		suffix: "for Flashcards",
+	},
+	{
+		activity: "mc-quiz",
+		suffix: "for Multiple Choice Quiz",
+	},
+	{
+		activity: "matching-activity",
+		suffix: "for Term Matching",
+	},
+	{
+		activity: "contents-of",
+		suffix: "to See The Contents of",
+	},
 ];
 
 function Selection({ params }: { params: { activity: string } }) {
-  const { status, data, error } = useQuery({
-    queryKey: ["collection"],
-    queryFn: fetchAllContent,
-  });
+	const { status, data, error } = useQuery({
+		queryKey: ["collection"],
+		queryFn: fetchAllContent,
+	});
 
-  const chapters = Object.values(Chapters);
-  const topics = Object.values(Topics);
-  const router = useRouter();
-  const selectedSuffix = titleSuffixes.find((item) => {
-    if (item.activity === params.activity) return item.suffix;
-  });
+	const chapters = Object.values(Chapters);
+	const topics = Object.values(Topics);
+	const router = useRouter();
+	const selectedSuffix = titleSuffixes.find((item) => {
+		if (item.activity === params.activity) return item.suffix;
+	});
 
-  const [selection, setSelection] = useLocalStorage("selection", [
-    chapters[0].valueOf(),
-    topics[0].valueOf(),
-  ]);
-  const [selectedChapter, setSelectedChapter] = useState<string>(chapters[0]);
-  const [selectedTopic, setSelectedTopic] = useState<string>(topics[0]);
+	const [selection, setSelection] = useLocalStorage("selection", [
+		chapters[0].valueOf(),
+		topics[0].valueOf(),
+	]);
+	const [selectedChapter, setSelectedChapter] = useState<string>(chapters[0]);
+	const [selectedTopic, setSelectedTopic] = useState<string>(topics[0]);
 
-  const [errorBoxText, setErrorBoxText] = useState("");
-  const [showError, setShowError] = useState(false);
+	const [errorBoxText, setErrorBoxText] = useState("");
+	const [showError, setShowError] = useState(false);
 
-  const [collection, setCollection] = useState<any[]>([]);
-  const [showModal, setShowModal] = useState(false);
+	const [collection, setCollection] = useState<any[]>([]);
+	const [showModal, setShowModal] = useState(false);
 
-  useEffect(() => {
-    // Gets chapter and topic from localstorage and sets the relevant hooks (used for remembering
-    // chapter and topic the user previously selected)
-    if (selection) {
-      setSelectedChapter(selection[0]);
-      setSelectedTopic(selection[1]);
-    }
-  }, []);
+	useEffect(() => {
+		// Gets chapter and topic from localstorage and sets the relevant hooks (used for remembering
+		// chapter and topic the user previously selected)
+		if (selection) {
+			setSelectedChapter(selection[0]);
+			setSelectedTopic(selection[1]);
+		}
+	}, [selection]);
 
-  // Updates the contents based on the result of the react query
-  useEffect(() => {
-    if (data && Array.isArray(data)) {
-      setCollection(data);
-    }
-  }, [data]);
+	// Updates the contents based on the result of the react query
+	useEffect(() => {
+		if (data && Array.isArray(data)) {
+			setCollection(data);
+		}
+	}, [data]);
 
-  // If user selection is valid, updates localstorage on user's chapter and topic choice, then changes the page
-  const submitForm = (event: React.FormEvent) => {
-    event.preventDefault();
-    if (isSelectionValid(selectedChapter, selectedTopic)) {
-      setSelection([selectedChapter, selectedTopic]);
-      router.push(
-        "/pages/" +
-          params.activity +
-          "/" +
-          selectedChapter +
-          "/" +
-          selectedTopic,
-      );
-    } else {
-      setErrorBoxText(
-        "Current chapter and topic selection is not valid, please change one or more selection",
-      );
-      setShowError(true);
-    }
-  };
+	// If user selection is valid, updates localstorage on user's chapter and topic choice, then changes the page
+	const submitForm = (event: React.FormEvent) => {
+		event.preventDefault();
+		if (isSelectionValid(selectedChapter, selectedTopic)) {
+			setSelection([selectedChapter, selectedTopic]);
+			router.push(
+				"/pages/" +
+					params.activity +
+					"/" +
+					selectedChapter +
+					"/" +
+					selectedTopic
+			);
+		} else {
+			setErrorBoxText(
+				"Current chapter and topic selection is not valid, please change one or more selection"
+			);
+			setShowError(true);
+		}
+	};
 
-  // Returns whether there is content for the selected chapter and topic
-  const isSelectionValid = (chapter: string, topic: string): boolean => {
-    const chapterKey = chapter.toLowerCase().replaceAll(" ", "");
-    const topicKey = topic.toLowerCase();
+	// Returns whether there is content for the selected chapter and topic
+	const isSelectionValid = (chapter: string, topic: string): boolean => {
+		const chapterKey = chapter.toLowerCase().replaceAll(" ", "");
+		const topicKey = topic.toLowerCase();
 
-    if (
-      collection &&
-      collection.find((doc: any) => {
-        if (doc[chapterKey] && doc[chapterKey][topicKey]) {
-          return true;
-        }
-      })
-    ) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+		if (
+			collection &&
+			collection.find((doc: any) => {
+				if (doc[chapterKey] && doc[chapterKey][topicKey]) {
+					return true;
+				}
+			})
+		) {
+			return true;
+		} else {
+			return false;
+		}
+	};
 
-  // Toggles whether the modal is visible
-  const toggleShowModal = () => {
-    setShowModal(!showModal);
-  };
+	// Toggles whether the modal is visible
+	const toggleShowModal = () => {
+		setShowModal(!showModal);
+	};
 
-  // Closes the error text box
-  const closeErrorBox = () => {
-    setShowError(false);
-  };
+	// Closes the error text box
+	const closeErrorBox = () => {
+		setShowError(false);
+	};
 
-  return (
-    <main className="main-center">
-      <h1 className="text-5xl font-semibold">
-        Select Chapter and Topic {selectedSuffix ? selectedSuffix.suffix : ""}
-      </h1>
-      <div>
-        <form className="flex flex-col mt-6" onSubmit={submitForm}>
-          <div className={styles.selectArea}>
-            <label htmlFor="chapter" className="text-xl">
-              Chapter
-            </label>
-            <select
-              id="chapter"
-              value={selectedChapter}
-              onChange={(e) => {
-                setSelectedChapter(e.target.value);
-              }}
-              className={styles.select}
-            >
-              {chapters.map((item) => (
-                <option key={item} value={item} className={styles.option}>
-                  {item}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className={styles.selectArea}>
-            <label htmlFor="topic" className="text-xl">
-              Topic
-            </label>
-            <select
-              id="topic"
-              value={selectedTopic}
-              onChange={(e) => {
-                setSelectedTopic(e.target.value);
-              }}
-              className={styles.select}
-            >
-              {topics.map((item) => (
-                <option key={item} value={item} className={styles.option}>
-                  {item}
-                </option>
-              ))}
-            </select>
-          </div>
-          <button
-            className="bg-slate-500 hover:bg-slate-700 text-white text-xl font-bold py-1 px-2 rounded-full mt-20"
-            data-test="submit-button"
-            disabled={collection.length == 0 ? true : false}
-          >
-            Submit
-          </button>
-          <button
-            className="bg-slate-500 hover:bg-slate-700 text-white text-xl font-bold py-1 px-2 rounded-full mt-10"
-            onMouseUp={toggleShowModal}
-            type="button"
-            data-test="valid-topics-button"
-          >
-            See Valid Topics
-          </button>
-        </form>
-      </div>
-      {showModal && (
-        <TopicStatusModal
-          onClose={toggleShowModal}
-          isSelectionValid={isSelectionValid}
-        />
-      )}
-      {showError && <ErrorBox text={errorBoxText} onClose={closeErrorBox} />}
-    </main>
-  );
+	return (
+		<main className="main-center">
+			<h1 className="text-5xl font-semibold">
+				Select Chapter and Topic {selectedSuffix ? selectedSuffix.suffix : ""}
+			</h1>
+			<div>
+				<form className="flex flex-col mt-6" onSubmit={submitForm}>
+					<div className={styles.selectArea}>
+						<label htmlFor="chapter" className="text-xl">
+							Chapter
+						</label>
+						<select
+							id="chapter"
+							value={selectedChapter}
+							onChange={(e) => {
+								setSelectedChapter(e.target.value);
+							}}
+							className={styles.select}
+						>
+							{chapters.map((item) => (
+								<option key={item} value={item} className={styles.option}>
+									{item}
+								</option>
+							))}
+						</select>
+					</div>
+					<div className={styles.selectArea}>
+						<label htmlFor="topic" className="text-xl">
+							Topic
+						</label>
+						<select
+							id="topic"
+							value={selectedTopic}
+							onChange={(e) => {
+								setSelectedTopic(e.target.value);
+							}}
+							className={styles.select}
+						>
+							{topics.map((item) => (
+								<option key={item} value={item} className={styles.option}>
+									{item}
+								</option>
+							))}
+						</select>
+					</div>
+					<button
+						className="bg-slate-500 hover:bg-slate-700 text-white text-xl font-bold py-1 px-2 rounded-full mt-20"
+						data-test="submit-button"
+						disabled={collection.length == 0 ? true : false}
+					>
+						Submit
+					</button>
+					<button
+						className="bg-slate-500 hover:bg-slate-700 text-white text-xl font-bold py-1 px-2 rounded-full mt-10"
+						onMouseUp={toggleShowModal}
+						type="button"
+						data-test="valid-topics-button"
+					>
+						See Valid Topics
+					</button>
+				</form>
+			</div>
+			{showModal && (
+				<TopicStatusModal
+					onClose={toggleShowModal}
+					isSelectionValid={isSelectionValid}
+				/>
+			)}
+			{showError && <ErrorBox text={errorBoxText} onClose={closeErrorBox} />}
+		</main>
+	);
 }
 
 export default Selection;
